1cfe38c3594d7e3441b1a97ac6786ebf2b0c34fe
Joerg Bornemann <github@jbornemann.de>
2023-10-06 22:48:23 +0200

    Add rules.mk from the Oryx-generated source

--- keyboards/ergodox_ez/shine/keymaps/jobor/rules.mk
+++ keyboards/ergodox_ez/shine/keymaps/jobor/rules.mk
@@ -1,2 +1,8 @@
+# Set any rules.mk overrides for your specific keymap here.
+# See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
+LTO_ENABLE = yes
 RGBLIGHT_ENABLE = yes
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+SPACE_CADET_ENABLE = no
 CAPS_WORD_ENABLE = yes
