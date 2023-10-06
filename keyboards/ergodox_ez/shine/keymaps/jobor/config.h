716d3e17600695319e3edf8a02245999b96a0a20
Joerg Bornemann <github@jbornemann.de>
2023-10-06 22:48:23 +0200

    Add config.h from Oryx source export

---
+++ keyboards/ergodox_ez/shine/keymaps/jobor/config.h
@@ -0,0 +1,7 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#define USB_SUSPEND_WAKEUP_DELAY 0
+#define FIRMWARE_VERSION u8"schnusel"
+#define LAYER_STATE_8BIT
