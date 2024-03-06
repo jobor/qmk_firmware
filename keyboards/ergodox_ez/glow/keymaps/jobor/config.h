9d5bb01a5792157258b394bfb5172cb64f9a13a4
Joerg Bornemann <github@jbornemann.de>
2024-03-06 22:17:07 +0100

    Add "German" layer to the Ergodox-EZ Glow
    ...and jump through some hoops to still be able to use Oryx for
    configuring the layouts.
    
    The unaltered Oryx-generated source code is now placed in the
    oryx_source subdirectory. The local keymap.c has the code for entering
    German characters via the compose key.
    
    Since I found no good way to redefine the layout we're now using F21-F24
    for German characters.

--- keyboards/ergodox_ez/glow/keymaps/jobor/config.h
+++ keyboards/ergodox_ez/glow/keymaps/jobor/config.h
@@ -2,20 +2,6 @@
   Set any config.h overrides for your specific keymap here.
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
-#define ORYX_CONFIGURATOR
-#define USB_SUSPEND_WAKEUP_DELAY 0
-#define FIRMWARE_VERSION u8"alDvV/pLbXb"
-#define RAW_USAGE_PAGE 0xFF60
-#define RAW_USAGE_ID 0x61
-#define LAYER_STATE_8BIT
-
-#define RGB_MATRIX_STARTUP_SPD 60
+#include "oryx_source/config.h"
 
 #define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
-#undef ENABLE_RGB_MATRIX_BAND_PINWHEEL_VAL
-#undef ENABLE_RGB_MATRIX_BAND_SPIRAL_VAL
-#undef ENABLE_RGB_MATRIX_RAINDROPS
-#undef ENABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
-#undef ENABLE_RGB_MATRIX_PIXEL_FRACTAL
-#undef ENABLE_RGB_MATRIX_PIXEL_FLOW
-#undef ENABLE_RGB_MATRIX_PIXEL_RAIN
--- keyboards/ergodox_ez/glow/keymaps/jobor/oryx_source/rules.mk
+++ keyboards/ergodox_ez/glow/keymaps/jobor/oryx_source/rules.mk
@@ -1,4 +1,8 @@
 # Set any rules.mk overrides for your specific keymap here.
 # See rules at https://docs.qmk.fm/#/config_options?id=the-rulesmk-file
-THIS_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
-include $(THIS_DIR)/oryx_source/rules.mk
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+ORYX_ENABLE = yes
+SPACE_CADET_ENABLE = no
+CAPS_WORD_ENABLE = yes
+LTO_ENABLE = yes
